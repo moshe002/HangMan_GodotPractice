343378cfde80f7f0adc46f42fc70880b0f1985fa
Moses <mosesfat@gmail.com>
2023-08-01 11:02:05 +0800

    added QnA and lowercase input

--- scripts/CheckButton.gd
+++ scripts/CheckButton.gd
@@ -2,10 +2,14 @@ extends Button
 # CHECK BUTTON TEXT/INPUT
 var inputText
 
+# gets the input text field node
 func _ready(): 
 	inputText = get_node("/root/GameScene/WhiteBackground/LineEdit")
 
 func _on_pressed():
-	if inputText != null:
-		var answer = inputText.text
-		print(answer)
+	if inputText != null: # checks if node exists
+		if inputText.text != "": # checks if node does have input
+			var answer = inputText.text.to_lower() 
+			print(answer)
+		else:
+			print("Empty field")
