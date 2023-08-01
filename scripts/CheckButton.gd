8f97530b223f3277c51c362b1c4577eb0a15ef88
Moses <mosesfat@gmail.com>
2023-08-01 21:20:15 +0800

    added questions and small changes

--- scripts/CheckButton.gd
+++ scripts/CheckButton.gd
@@ -1,15 +1,22 @@
 extends Button
 # CHECK BUTTON TEXT/INPUT
 var inputText
-
+var answer
 # gets the input text field node
 func _ready(): 
 	inputText = get_node("/root/GameScene/WhiteBackground/LineEdit")
+	answer = get_node("/root/GameScene/WhiteBackground/Question")
 
 func _on_pressed():
 	if inputText != null: # checks if node exists
 		if inputText.text != "": # checks if node does have input
-			var answer = inputText.text.to_lower() 
-			print(answer)
+			var userInput = inputText.text.to_lower() 
+			print(userInput)
+			var realAnswer = answer.finalData.answer.to_lower()
+			if realAnswer == userInput:
+				print("Correct answer")
+				#inputText.text = ""
+			else: 
+				print("Wrong answer")
 		else:
 			print("Empty field")
