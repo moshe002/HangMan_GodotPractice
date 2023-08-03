9f99b83fc95c8ca691b178f4f0c955ca19589f6a
Moses <mosesfat@gmail.com>
2023-08-03 15:12:29 +0800

    minor additions

--- scripts/CheckButton.gd
+++ scripts/CheckButton.gd
@@ -3,13 +3,17 @@ extends Button
 var content_as_dictionary
 var finalData
 var inputText
-var QuestionLabel
-var CategoryLabel
+var QuestionLabel 
+var CategoryLabel 
+var ResultLabel
+#var delayTimer
 # gets the input text field node
 func _ready(): 
 	inputText = get_node("/root/GameScene/WhiteBackground/LineEdit")
 	QuestionLabel = get_node("/root/GameScene/WhiteBackground/Question")
 	CategoryLabel = get_node("/root/GameScene/WhiteBackground/Category")
+	ResultLabel = get_node("/root/GameScene/WhiteBackground/ResultLabel")	
+	#delayTimer = get_node("/root/GameScene/WhiteBackground/Timer")
 	_get_data()
 
 func _on_pressed():
@@ -18,11 +22,19 @@ func _on_pressed():
 			var userInput = inputText.text.to_lower() 
 			print("User entered: " + userInput)
 			var realAnswer = finalData.answer.to_lower()
-			if realAnswer == userInput:
+			if realAnswer == userInput: # IF ANSWER IS CORRECT
 				print("Correct answer")
+				ResultLabel.visible = true 
+				ResultLabel.set("theme_override_colors/font_color", Color(0,1,0,1))
+				# Color(0, 1, 0, 1) green
+				ResultLabel.text = "Correct"
 				inputText.text = ""
 				_get_data()
-			else: 
+			else: # IF ANSWER IS WRONG
+				ResultLabel.visible = true
+				ResultLabel.set("theme_override_colors/font_color", Color(1, 0, 0, 1))
+				# Color(1, 0, 0, 1) red
+				ResultLabel.text = "Wrong"
 				print("Wrong answer")
 		else:
 			print("Empty field")
@@ -41,8 +53,10 @@ func _get_data():
 	content_as_dictionary = JSON.parse_string(content_as_text)
 	# content_as_dictionary[0].category, content_as_dictionary[0].question, content_as_dictionary[0].answer
 	finalData = content_as_dictionary[randi() % content_as_dictionary.size()] # this gets a random object in the array (JSON file)
+	# make it so that rare ra mo balik ang question kay so far magbalik2 siya frequently
 	QuestionLabel.text = finalData.question 
 	categoryLabel.text = finalData.category
 	print(finalData)
-	
-# make it so that rare ra mo balik ang question kay so far magbalik2 siya frequently
+				
+func _on_timer_timeout(): # make it so that it will display for 2 seconds and not go away immediately
+	ResultLabel.visible = !true
